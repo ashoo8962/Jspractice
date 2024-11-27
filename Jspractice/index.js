05070274b170c01efd4ef8afd6711f18b90b90ec
as <you@example.com>
2024-11-27 16:10:27 +0530

    added

--- Jspractice/index.js
+++ Jspractice/index.js
@@ -1,1 +1,24 @@
-hello
+
+console.log('hello world')
+document.body.firstElementChild
+// <div>Box1</div>
+document.body.firstElementChild.childNodes
+// [text ,div text ]
+              
+
+
+
+            //    container's chid
+document.body.firstElementChild.children  
+// [div.box,div,box,div.box]
+//will get html element nodes
+document.body.firstElementChild.children[1]
+// =>div.box <div className='box'>Box 2</div>
+
+document.body.firstElementChild.children[3].previousElementSibling
+{/* <div>Box3</div> */}
+// prev element 
+
+document.body.firstElementChild.children[3].previousSibling
+// text not element
+
